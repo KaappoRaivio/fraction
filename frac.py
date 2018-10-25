cf85d643d5ace1dfd2988415bb5d2b56d337c821
NoSocksForYou <kaappo.raivio@gmail.com>
2018-10-25 11:43:21 +0300

    autocommit

---
+++ frac.py
@@ -0,0 +1,90 @@
+
+import math
+
+
+class Frac:
+
+
+    def __init__(self, numerator: int, denominator: int):
+        self.numerator = numerator
+        self.denominator = denominator
+
+    def __expand(self, expander: int):
+
+        return Frac(self.numerator * expander, self.denominator * expander)
+
+    def __reduce(self, reducor):
+        if  self.numerator % reducor or self.denominator % reducor:
+            raise Exception("Can't reduce with this")
+        else:
+            return Frac(self.numerator // reducor, self.denominator // reducor)
+
+
+    def __add__(self, other):
+        if isinstance(other, Frac):
+            expandedself = self.__expand(self.__lcm(self.denominator, other.denominator) // self.denominator)
+            expandedother = other.__expand(self.__lcm(self.denominator, other.denominator) // other.denominator)
+
+            temp = Frac(expandedother.numerator + expandedself.numerator, expandedself.denominator)
+            return temp.__reduce(self.__gcd(self.numerator, self.denominator))
+
+        elif isinstance(other, int):
+            temp = Frac(self.numerator + other * self.denominator, self.denominator)
+            return temp.__reduce(self.__gcd(self.numerator, self.denominator))
+
+    def __mul__(self, other):
+        if isinstance(other, Frac):
+            temp = Frac(other.numerator * self.numerator, self.denominator * other.denominator)
+
+            return temp.__reduce(self.__gcd(self.numerator, self.denominator))
+
+        elif isinstance(other, int):
+            temp = Frac(self.numerator * other, self.denominator)
+            print(temp)
+
+            return temp.__reduce(self.__gcd(temp.numerator, temp.denominator))
+
+
+
+    def __truediv__(self, other):
+        pass
+
+    def __sub__(self, other):
+        pass
+
+    def __invert__(self):
+        return Frac(self.denominator, self.numerator)
+
+    def __float__(self):
+        return self.numerator / self.denominator
+
+    def __str__(self):
+        return f"Frac({self.numerator}, {self.denominator})"
+
+    @staticmethod
+    def __gcd(x, y):
+        print(x, y, math.gcd(x, y))
+        return math.gcd(x, y)
+
+
+    @staticmethod
+    def __lcm(x, y):
+        if x > y:
+            greater = x
+        else:
+            greater = y
+
+        while True:
+
+            if greater % x == 0 and greater % y == 0:
+                lcm = greater
+                break
+            greater += 1
+
+        return lcm
+
+
+a = Frac(3, 4)
+b = Frac(3, 4)
+
+print(a * 4)
